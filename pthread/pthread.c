7801cfa7582c8ab8628a56107a982c4c406bfdb3
Matthew Sinclair <msinclair@selene.beocat.ksu.edu>
2022-04-30 15:54:18 -0500

    fixed issues with Pthread. Effectively Done With File

--- pthread/pthread.c
+++ pthread/pthread.c
@@ -1,28 +1,26 @@
-#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <omp.h>
+#include <pthread.h>
 
-#include "pthread.h"
+#define MAX_LINES 500000
 
-#define MAX_LINES 1000005
-
-int findMinChars(char *line);
+int findMinChars(char *line, int len);
 void *thread_func(void *id);
 
-int Num_Threads = 8;
+int Num_Threads = 20;
 int *min_char;
 pthread_mutex_t mutex_min_char;
 
 /* comparison function to use to find the lowest character in each row */
-int findMinChars(char *line)
+int findMinChars(char *line, int len)
 {
     char compChar = '~';
-    for (int i = 0; i < strlen(line); i++)
+    int i;
+    for (i = 0; i < len; i++)
     {
-        if ((line[i] > 32) && (line[i] < 127)) //
+        if ((line[i] > 32) && (line[i] < 127)) //Everything Only between Space and Delete
         {
             if (line[i] < compChar)
             {
@@ -30,13 +28,13 @@ int findMinChars(char *line)
             }
         }
     }
-    return compChar;
+    return (int) compChar;
 }
 
 void *thread_func(void *id)
 {
     int line_num = 0;
-    char c;
+    int c;
     int t_id = *(int*)id;
     int i, j;
     int ret;
@@ -44,22 +42,24 @@ void *thread_func(void *id)
     fp = fopen("/homes/dan/625/wiki_dump.txt", "r");
     if (!fp)
         exit(-1);
-    char *line = malloc(sizeof(char) * 2000);
+    char *line = malloc(sizeof(char) * 3000);
+    if ( line == NULL ) {
+        printf("allocation Failure\n");
+        return (void*)-1;
+    }
     size_t line_len = sizeof(line);
 
     // Get to the starting line for the thread
-    for (i = 0; i <= t_id-1; i++)
+    for (i = 0; i <= t_id; i++)
     {
         line_num++;
-        if (getline(&line, &line_len, fp) == -1)
+        ret = getline(&line, &line_len, fp);
+            if (ret == -1)
             break;
     }
     // run function to get first minchar
-    c = findMinChars(line);
-
-    pthread_mutex_lock(&mutex_min_char);
-    min_char[line_num-1] = c;
-    pthread_mutex_unlock(&mutex_min_char);
+    c = findMinChars(line, ret);
+    min_char[(line_num-1)] = c;
 
     // run until we are out of lines
     for (i = 0; i < MAX_LINES; i++)
@@ -68,57 +68,62 @@ void *thread_func(void *id)
         {
             line_num++;
             ret = getline(&line, &line_len, fp);
-            if (ret == -1)
+            if (ret == -1 || line_num > MAX_LINES)
                 break; // this will only get us out of the inner loop
         }
-        if (ret == -1)
+        if (ret == -1 || line_num > MAX_LINES)
             break;
-        c = findMinChars(line);
-        pthread_mutex_lock(&mutex_min_char);
-        min_char[line_num-1] = c;
-        pthread_mutex_unlock(&mutex_min_char);
+        c = findMinChars(line, ret);
+        min_char[(line_num-1)] = c;
     }
 
-    pthread_exit(NULL);
+    free(line);
+    free(id);
+    fclose(fp);
+    pthread_exit(0);
 }
 
 int main()
 {
     int i;
-    min_char = malloc(sizeof(int) * MAX_LINES);
-
+    int ret;
     pthread_t threads[Num_Threads];
-    pthread_attr_t attr;
+    min_char = malloc(sizeof(int) * MAX_LINES);
     void *status;
-    pthread_attr_init(&attr);
-    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-    pthread_mutex_init(&mutex_min_char, NULL);
 
-    for (i = 0; i <= Num_Threads; i++)
-    {
-        if (pthread_create(&threads[i], &attr, thread_func, (void *)&i))
-        {
-            printf("pthread create error:");
-            return -1;
-        }
-    }
-
-    pthread_attr_destroy(&attr);
     for (i = 0; i < Num_Threads; i++)
     {
-        if (pthread_join(threads[i], &status))
+        int *arg = malloc(sizeof(*arg));
+        if ( arg == NULL ) {
+            printf("allocation Failure\n");
+            return -1;
+        }
+        *arg = i;
+
+        ret = pthread_create(&threads[i], NULL, thread_func, (void *)arg);
+        if (ret != 0)
         {
-            printf("pthread join error:");
+            printf("pthread create error:\n");
             return -1;
         }
     }
-    pthread_mutex_destroy(&mutex_min_char);
 
+    for (i = 0; i < Num_Threads; i++)
+    {
+        ret = pthread_join(threads[i], &status);
+        if (ret != 0)
+        {
+            printf("pthread join error: %d\n", ret);
+            return -1;
+        }
+    }
+    
     for (i = 0; i < MAX_LINES; i++)
     {
         int val = min_char[i];
-        printf("%d: %d\n", i, val);
+        printf("%d: %d\n", i+1, val);
     }
-
+    
+    free(min_char);
     return 0;
 }
