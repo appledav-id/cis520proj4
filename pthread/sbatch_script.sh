5789221e9deaff39b2e1c91ebd8db50c8849a333
Matthew Sinclair <msinclair@selene.beocat.ksu.edu>
2022-04-30 22:37:23 -0500

    Added some finishing features to Attempt of pthread

--- pthread/sbatch_script.sh
+++ pthread/sbatch_script.sh
@@ -9,10 +9,10 @@
 ## be #!/bin/bash (or another valid shell).
 
 ## Specify the amount of RAM needed _per_core_. Default is 1G
-#SBATCH --mem-per-cpu=1G
+#SBATCH --mem-per-cpu=2G
 
 ## Specify the maximum runtime. Default is 1 hour (1:00:00)
-#SBATCH --time=0:10:00   # Use the form DD-HH:MM:SS
+#SBATCH --time=0:30:00   # Use the form DD-HH:MM:SS
 ##SBATCH --partition=killable.q   # Job may run as killable on owned nodes
 
 ## Tell slurm how many cores to use, and how many nodes to use
@@ -21,7 +21,6 @@
 ## benefit from emailing us at beocat@cis.ksu.edu to see how we can assist in
 ## getting your job scheduled in a reasonable amount of time.
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=20
 
 ## Name my job, to make it easier to find in the queue
 #SBATCH --job-name=mls_pthread
@@ -32,4 +31,10 @@
 ## Email address to send the email to based on the above line.
 #SBATCH --mail-user=msinclair@ksu.edu
 
-./pthread
+./pthread $1 1000
+
+./pthread $1 10000
+
+./pthread $1 100000
+
+./pthread $1 1000000
